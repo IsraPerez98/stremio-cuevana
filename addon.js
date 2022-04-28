0151114b98d03f9727ea68ffa9aec010f2768dca
IsraPerez98 <isra1998isra@gmail.com>
2022-04-27 23:32:33 -0400

    parallel stuff

--- addon.js
+++ addon.js
@@ -1,6 +1,7 @@
 const { addonBuilder } = require("stremio-addon-sdk");
 const scrapping = require("./scrapping");
 const cuevanaapi = require("./cuevanaapi");
+const puppeteer = require('puppeteer');
 
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
@@ -22,6 +23,26 @@ const manifest = {
 };
 const builder = new addonBuilder(manifest);
 
+async function getStreamObject(browser, url, language){
+	try {
+		const stream = await scrapping.getStreamFromCuevana(browser, url);
+		const streamObject = {
+			"url": stream,
+			"description": language,
+			behaviorHints: {
+				notWebReady: true,
+			}
+		}
+
+		return streamObject;
+	}
+	catch(error) {
+		console.log(error);
+		return {"url": ""};
+	}
+
+}
+
 
 builder.defineStreamHandler( async ({type, id}) => {
 	console.log("request for streams: "+type+" "+id+" ");
@@ -32,24 +53,20 @@ builder.defineStreamHandler( async ({type, id}) => {
 
 		const movieURLs = await cuevanaapi.getMovieURLs(type,id);
 
-		let streams = [];
+		let promises = [];
 
-		//console.log(movieURLs[0]);
+		//we need a puppeteer browser to handle some redirects
+		const browser = await puppeteer.launch({ headless: true });
 
 		for(const language in movieURLs[0]) {
 			for (const source of movieURLs[0][language]) {
-				const stream = await scrapping.getStreamFromCuevana(source.url);
-				console.log({stream});
-				streams.push({
-					"url": stream,
-					"name": language,
-					"description": language,
-					behaviorHints: {
-						notWebReady: true,
-					}
-				})
+				promises.push(getStreamObject(browser, source.url, language));
 			}
-		} 
+		}
+		
+		const streams = await Promise.all(promises);
+
+		browser.close();
 
 		return {streams: streams};
 		
--- scrapping.js
+++ scrapping.js
@@ -1,40 +1,47 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
 
-async function getStreamPage(url) {
+async function getStreamPage(browser,url) {
 
 	try {
-        const browser = await puppeteer.launch({ headless: true });
+        //const browser = await puppeteer.launch({ headless: true });
         const page = await browser.newPage();
         
         await page.goto(url, {waitUntil: 'networkidle2'});
 
         //wait for the player to load
-        await page.waitForSelector('#player');
+        await page.waitForSelector('#player', {timeout: 5000});
 
-        console.log(page.url());
+        //console.log(page.url());
 
-        //await page.screenshot({path: 'screenshot.png'});
         
-        
-        browser.close();
+        //browser.close();
 
         return page.url();
+
     } catch(error) {
+        if (error instanceof puppeteer.errors.TimeoutError) {
+            //console.log("Timeout error");
+            return "";
+        }
         console.log(error);
     }
 }
 
-async function getStreamFromTomatomatela(url) {
+async function getStreamFromTomatomatela(browser, url) {
 
-    console.log("Converting from Tomatomatela");
+    //console.log("Converting from Tomatomatela");
     
     //sometimes the url contains player.php with a play button in the middle
     url = url.replace("player.php", "goto.php");
 
     //console.log("url: ", url);
 
-    let streamPage = await getStreamPage(url);
+    let streamPage = await getStreamPage(browser, url);
+
+    if ( streamPage === "" ) {
+        return "";
+    }
 
     //this is what the site uses to get the file from the url
 
@@ -54,7 +61,10 @@ async function getStreamFromTomatomatela(url) {
 
     const stream = response.data.file;
 
-    console.log("stream: ", stream);
+    console.log({
+        "url": url,
+        "stream": stream,
+    });
 
 
     return stream;
@@ -62,14 +72,14 @@ async function getStreamFromTomatomatela(url) {
 
 }
 
-async function getStreamFromCuevana(url) {
+async function getStreamFromCuevana(browser, url) {
     //gets a url from cuevana3 and converts it to a usable stream for Stremio
     //i hope there was a better way of doing this
 
-    console.log("Converting url to stream: ", url);
+    //console.log("Converting url to stream: ", url);
 
     if(url.includes("tomatomatela")) {
-        return await getStreamFromTomatomatela(url);
+        return await getStreamFromTomatomatela(browser, url);
     } else {
         console.log("unknown url", url);
         return "";
