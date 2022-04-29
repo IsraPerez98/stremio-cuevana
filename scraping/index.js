deadcc05ee60825a4f8044605307d03930789213
IsraPerez98 <isra1998isra@gmail.com>
2022-04-28 22:54:19 -0400

    fembed support

--- scraping/index.js
+++ scraping/index.js
@@ -1,8 +1,11 @@
 const tomatomatela = require("./tomatomatela");
+const fembed = require("./fembed");
 
 async function ObtenerStream(browser, url) {
     if(url.includes('tomatomatela')) {
         return tomatomatela.ObtenerStream(browser, url);
+    } else if(url.includes('fembed')) {
+        return fembed.ObtenerStream(browser, url);
     } else {
         console.log("URL no disponible: "+url);
         return "";
